0cc353826b05dbafce8f50c0986038362c0ef6fa
方便面 <fangletian2008@outlook.com>
2026-09-12 17:53:00 +0800

    test: complete task repository editing double

--- shared/application/src/commonTest/kotlin/dev/agenticscheduler/application/editing/EditingTest.kt
+++ shared/application/src/commonTest/kotlin/dev/agenticscheduler/application/editing/EditingTest.kt
@@ -280,6 +280,7 @@ private class FakeTaskRepository : TaskRepository {
     override fun observeFocusBlocks(): Flow<ImmutableList<FocusBlock>> = focusFlow
     override suspend fun getFocusBlock(id: FocusBlockId): FocusBlock? = null
     override suspend fun upsertFocusBlock(focusBlock: FocusBlock) = Unit
+    override suspend fun deleteFocusBlock(id: FocusBlockId) = Unit
     override fun observeWorkLogs(): Flow<ImmutableList<WorkLog>> = workLogFlow
     override suspend fun getWorkLog(id: WorkLogId): WorkLog? = null
     override suspend fun upsertWorkLog(workLog: WorkLog) = Unit
